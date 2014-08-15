720eaae1c77119a2f3c8ce8b84d25e77221f3cc7
Vitalik <ananichev.v.v@gmail.com>
2014-08-15 21:04:09 +0300

    Create border

--- app/controllers/puzzle_pages_controller.rb
+++ app/controllers/puzzle_pages_controller.rb
@@ -1,9 +1,20 @@
 class PuzzlePagesController < ApplicationController
 	
   def index
+  	@new_game = true
   end
   
   def play
+    prepare_board
   end
+  
+  private
+  
+	def prepare_board
+	  @elem = (1..15).to_a.shuffle
+  	  @board = []
+  	  @elem.each_slice(4) { |e| @board << e }
+  	  @board[3] << 0
+	end
 
 end
