405e447fe0559c9a0607aafee0f53919cc498de3
Vitalii Ananichev <ananichev@users.noreply.github.com>
2014-08-17 16:01:15 +0300

    Update puzzle_pages_controller.rb

--- app/controllers/puzzle_pages_controller.rb
+++ app/controllers/puzzle_pages_controller.rb
@@ -3,45 +3,44 @@ class PuzzlePagesController < ApplicationController
   after_action :prepare_board, only: :index
 	
   def index
-  	session[:board] = nil
+    session[:board] = nil
   end
   
   def play
-  	@board = session[:board]
+    @board = session[:board]
   end
   
   def move_elem
-  	@elements = session[:board].flatten
-  	congratulations(@elements)
+    @elements = session[:board].flatten
+    congratulations(@elements)
     elem = params[:elem].to_i
-  	zero = @elements.index(0)
-  	moved = @elements.index(elem)
-  	@elements[zero] = @elements[moved]
-  	@elements[moved] = 0
-  	@board = []
-  	@elements.each_slice(4) { |e| @board << e }
-  	session[:board] = @board
-  	respond_to do |format|
-  	  format.html { redirect_to play_path }
-  	  format.js
-  	end
-  	
+    zero = @elements.index(0)
+    moved = @elements.index(elem)
+    @elements[zero] = @elements[moved]
+    @elements[moved] = 0
+    @board = []
+    @elements.each_slice(4) { |e| @board << e }
+    session[:board] = @board
+    respond_to do |format|
+      format.html { redirect_to play_path }
+      format.js
+    end
   end
   
   private
   
-	def prepare_board
-	  @elem = (1..15).to_a.shuffle
-  	  @board = []
-  	  @elem.each_slice(4) { |e| @board << e }
-  	  @board[3] << 0
-  	  session[:board] = @board
-	end
+    def prepare_board
+      @elem = (1..15).to_a.shuffle
+      @board = []
+      @elem.each_slice(4) { |e| @board << e }
+      @board[3] << 0
+      session[:board] = @board
+    end
 	
-	def congratulations(array)
-	  if array == (1..15).to_a << 0
-	    flash[:notice] = "Congratulations! You win"	
-  	  end
-	end
+    def congratulations(array)
+      if array == (1..15).to_a << 0
+        flash[:notice] = "Congratulations! You win"	
+      end
+    end
 	
 end
