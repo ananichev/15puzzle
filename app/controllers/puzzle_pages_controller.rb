d1c0fb7254cf830f641ec8d027b08844cc6f7dcc
Vitalik <ananichev.v.v@gmail.com>
2014-08-17 15:23:41 +0300

    Trying to add ajax

--- app/controllers/puzzle_pages_controller.rb
+++ app/controllers/puzzle_pages_controller.rb
@@ -12,6 +12,7 @@ class PuzzlePagesController < ApplicationController
   
   def move_elem
   	@elements = session[:board].flatten
+  	congratulations(@elements)
     elem = params[:elem].to_i
   	zero = @elements.index(0)
   	moved = @elements.index(elem)
@@ -20,7 +21,11 @@ class PuzzlePagesController < ApplicationController
   	@board = []
   	@elements.each_slice(4) { |e| @board << e }
   	session[:board] = @board
-  	redirect_to play_path
+  	respond_to do |format|
+  	  format.html { redirect_to play_path }
+  	  format.js
+  	end
+  	
   end
   
   private
@@ -31,6 +36,12 @@ class PuzzlePagesController < ApplicationController
   	  @elem.each_slice(4) { |e| @board << e }
   	  @board[3] << 0
   	  session[:board] = @board
+	end
+	
+	def congratulations(array)
+	  if array == (1..15).to_a << 0
+	    flash[:notice] = "Congratulations! You win"	
+  	  end
 	end
 	
 end
--- app/helpers/puzzle_pages_helper.rb
+++ app/helpers/puzzle_pages_helper.rb
@@ -1,29 +1,36 @@
 module PuzzlePagesHelper
-	
-  def moving_elements(board, part)
-  	board = board.flatten
-	zero = board.index(0)
-  	pos(board)
-	
-  	unless part.include? 0
-  	  @left_elem = zero - 100
-  	  @right_elem = zero + 100
-  	end
-  	
-  	moving_elem = []
-  	moving_elem << board[@upper_elem] << board[@lower_elem] << board[@left_elem] << board[@right_elem]
 
-  	moving_elem.compact!
-  	p moving_elem
-  end
+  private
+	
+    def moving_elements(board, part)
+  	  moving_elem = []
+  	  board = board.flatten
+	  zero = board.index(0)
+  	  positions(board)
+	  horizontal_elem(part, board)
+	  vertical_elem(board) 	
+  	  (moving_elem << board[@upper_elem] << board[@lower_elem] << board[@left_elem] << board[@right_elem]).compact!
+  	  moving_elem
+    end
   
-  def pos(board)
-	zero = board.index(0)
-  	@left_elem = zero - 1
-  	@right_elem = zero + 1
-  	@upper_elem = zero - 4
-  	@lower_elem = zero + 4
-  end
+    def positions(board)
+	  zero = board.index(0)
+  	  @left_elem = zero - 1
+  	  @right_elem = zero + 1
+  	  @upper_elem = zero - 4
+  	  @lower_elem = zero + 4
+    end
   
-
+    def horizontal_elem(part, board)
+  	  unless part.include? 0
+  	    @left_elem = board.index(0) - 100
+  	    @right_elem = board.index(0) + 100
+  	  end
+    end
+    
+    def vertical_elem(board)
+	  if @upper_elem < 0
+		@upper_elem = board.index(0) + 100
+      end
+    end
 end
