3add37059f8c1dc2841b6dfe80233679c9bce966
ananichev <va@anahoret.com>
2014-08-18 16:48:41 +0300

    Add vertical method in puzzle pages controller

--- app/controllers/puzzle_pages_controller.rb
+++ app/controllers/puzzle_pages_controller.rb
@@ -7,17 +7,33 @@ class PuzzlePagesController < ApplicationController
   end
   
   def play
-    @board = session[:board]
+    if params[:win]
+      @board = []
+      ((1..14).to_a << 0 << 15).each_slice(4) { |e| @board << e }
+      session[:board] = @board
+    else
+      @board = session[:board]
+    end
+    congratulations(@board)
   end
   
   def move_elem
     @elements = session[:board].flatten
-    congratulations(@elements)
     elem = params[:elem].to_i
     zero = @elements.index(0)
     moved = @elements.index(elem)
-    @elements[zero] = @elements[moved]
-    @elements[moved] = 0
+    l = [2, 3]
+    r = [-2, -3]
+    u = [-12, -8, 8, 12]
+    if l.include?(zero - moved)
+      @elements.insert(moved, 0).delete_at(zero + 1)
+    elsif r.include?(zero - moved)
+      @elements.insert(moved + 1, 0).delete_at(zero)
+    elsif u.include?(zero - moved)
+      @elements = vertical(session[:board], elem)
+    else
+      @elements[zero], @elements[moved] = @elements[moved], 0
+    end
     @board = []
     @elements.each_slice(4) { |e| @board << e }
     session[:board] = @board
@@ -26,7 +42,7 @@ class PuzzlePagesController < ApplicationController
       format.js
     end
   end
-  
+
   private
   
     def prepare_board
@@ -38,9 +54,61 @@ class PuzzlePagesController < ApplicationController
     end
 	
     def congratulations(array)
-      if array == (1..15).to_a << 0
+      if array.flatten == (1..15).to_a << 0
         flash[:notice] = "Congratulations! You win"	
       end
     end
+
+  def vertical(board, elem)
+    @last = true  if board.flatten.index(0) < board.flatten.index(elem)
+
+    unless @last
+      board.each do |part|
+        if part.include? elem
+          @index = part.index(elem)
+          if board.index(part) == 3
+            @last = true
+            break
+          end
+          part[@index] = 0
+        else
+          if @index
+            if part.include? 0
+              part[@index] = elem
+              break
+            else
+              i = part[@index]
+              part[@index] = elem
+              elem = i
+            end
+          end
+        end
+      end
+    end
+
+    if @last
+      board.reverse!
+      board.each do |part|
+        if part.include? elem
+          @index = part.index(elem)
+          part[@index] = 0
+        else
+          if @index
+            if part.include? 0
+              part[@index] = elem
+              break
+            else
+              i = part[@index]
+              part[@index] = elem
+              elem = i
+            end
+          end
+        end
+      end
+      board.reverse!
+    end
+    board.flatten!
+  end
 	
 end
+
--- app/helpers/puzzle_pages_helper.rb
+++ app/helpers/puzzle_pages_helper.rb
@@ -3,34 +3,11 @@ module PuzzlePagesHelper
   private
 	
     def moving_elements(board, part)
-      moving_elem = []
       board = board.flatten
       zero = board.index(0)
-      positions(board)
-      horizontal_elem(part, board)
-      vertical_elem(board) 	
-      (moving_elem << board[@upper_elem] << board[@lower_elem] << board[@left_elem] << board[@right_elem]).compact!
-      moving_elem
-    end
-  
-    def positions(board)
-      zero = board.index(0)
-      @left_elem = zero - 1
-      @right_elem = zero + 1
-      @upper_elem = zero - 4
-      @lower_elem = zero + 4
-    end
-  
-    def horizontal_elem(part, board)
-      unless part.include? 0
-      @left_elem = board.index(0) - 100
-      @right_elem = board.index(0) + 100
-      end
-    end
-    
-    def vertical_elem(board)
-      if @upper_elem < 0
-        @upper_elem = board.index(0) + 100
-      end
+      upper_elem = [board[zero - 4], board[zero - 8], board[zero - 12]].each { |e| e < 0 ? nil : e }
+      lower_elem = [board[zero + 4], board[zero + 8], board[zero + 12]]
+      line = part if part.include? 0
+      [upper_elem, lower_elem, line].flatten.compact
     end
 end
